fd48ea19ac3a9b483b0ba88cf1aa13ac4b81af06
emmanuel-lbs <59801700+emmanuel-lbs@users.noreply.github.com>
2023-01-05 13:48:12 +0100

    begin of database v2

--- back/src/app.module.ts
+++ back/src/app.module.ts
@@ -1,9 +1,21 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { User } from 'entity/entity';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: 'admin',
+      password: 'admin',
+      entities: [User],
+      synchronize: true,
+    })
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
