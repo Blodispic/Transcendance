353fb864186fc33e7662a3fe14d49a346086692f
Blodispic <75845611+Blodispic@users.noreply.github.com>
2023-01-06 17:58:22 +0100

    Update app.module.ts

--- back/src/app.module.ts
+++ back/src/app.module.ts
@@ -1,15 +1,8 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
-<<<<<<< HEAD
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { User } from './user/entities/user.entity';
-=======
-import { User } from './user/user.entity';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
-import { UserService } from './user/user.service';
->>>>>>> 90bad53df266c7fc5d1bec75c40b7e3b820448b4
 import { UserModule } from './user/user.module';
 @Module({
   imports: [
@@ -22,11 +15,7 @@ import { UserModule } from './user/user.module';
       entities: [User],
       synchronize: true,
     }),
-<<<<<<< HEAD
     UserModule,
-=======
-    UserModule
->>>>>>> 90bad53df266c7fc5d1bec75c40b7e3b820448b4
   ],
   controllers: [AppController],
   providers: [AppService, UserService],
