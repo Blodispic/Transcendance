28556aa6c20966701d4406c6499e65cce2f66706
emmanuel-lbs <59801700+emmanuel-lbs@users.noreply.github.com>
2023-01-06 12:30:52 +0100

    database user

---
+++ back/src/users/users.entity.ts
@@ -0,0 +1,19 @@
+
+import { Entity, Column, PrimaryGeneratedColumn } from 'typeorm';
+
+@Entity()
+export class User {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column()
+  firstName: string;
+
+  @Column()
+  lastName: string;
+
+  @Column({ default: true })
+  isActive: boolean;
+
+  // ON vera le reste plus tard
+}
