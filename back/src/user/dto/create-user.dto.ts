f88330b6b96c1a1db137e575ed1c9c4897c6c557
Blodispic <75845611+Blodispic@users.noreply.github.com>
2023-01-17 00:22:16 +0100

    Added class-validator to create_user

--- back/src/user/dto/create-user.dto.ts
+++ back/src/user/dto/create-user.dto.ts
@@ -1,5 +1,15 @@
+import { IsString, IsEmail, IsNotEmpty } from 'class-validator';
+
 export class CreateUserDto {
+	@IsString()
+	@IsNotEmpty()
 	readonly username: string;
+	
+	@IsEmail()
+	@IsNotEmpty()
 	readonly password: string;
+	
+	@IsString()
+	@IsNotEmpty()
 	readonly email: string;
 }
