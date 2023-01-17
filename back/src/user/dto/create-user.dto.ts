400acf0a0a1fb818dbd6b661c97603b2ad46b02a
Blodispic <75845611+Blodispic@users.noreply.github.com>
2023-01-17 13:45:26 +0100

    Old one

--- back/src/user/dto/create-user.dto.ts
+++ back/src/user/dto/create-user.dto.ts
@@ -1,15 +1,5 @@
-import { IsString, IsEmail, IsNotEmpty } from 'class-validator';
-
 export class CreateUserDto {
-	@IsString()
-	@IsNotEmpty()
 	readonly username: string;
-	
-	@IsEmail()
-	@IsNotEmpty()
 	readonly password: string;
-	
-	@IsString()
-	@IsNotEmpty()
 	readonly email: string;
 }
