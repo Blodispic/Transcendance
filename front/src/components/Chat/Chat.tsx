e603fb6e0b651cd7bae323b54917f829d6e074c9
Seoyoung Lee <selee@student.42lyon.fr>
2023-01-18 16:35:12 +0100

    commit before pulling chat/back

--- front/src/components/Chat/Chat.tsx
+++ front/src/components/Chat/Chat.tsx
@@ -3,69 +3,73 @@ import * as React from "react";
 import { io } from "socket.io-client";
 import '../../styles/chat.scss'
 
-// const socket = io("http://localhost:3000")
+const socket = io("http://localhost:3000")
 
-// const message = document.getElementById('message');
-// const messages = document.getElementById('messages');
+/*
+FRONT -> emit message -> BACK
+*/
 
-// const handleSubmitNewMessage = () => {
-// 	socket.emit('message', { data: message })
-// }
+function ChannelList() {
+	return <ul>
+		<li>channel 1</li>
+		<li>channel 2</li>
+	</ul>
+}
 
-// socket.on('message', ({ data }) => {
-// 	handleNewMessage(data);
-// })
+function DMList() {
+	return <ul>
+		<li>DM 1</li>
+		<li>DM 2</li>
+	</ul>
+}
 
-// const handleNewMessage = (message: string) => {
-// 	messages?.appendChild(buildNewMessage(message));
-// }
+function UserList() {
+	return 	<ul>
+		<li> user1 </li>
+		<li> user2 </li>
+		<li> user3 </li>
+	</ul>
+}
 
-// const buildNewMessage = (message: string) => {
-// 	const li = document.createElement("li");
-// 	li.appendChild(document.createTextNode(message))
-// 	return li;
-// }
+function Chat(this: any) {
 
+const [inputMessage, setInputMessage] = React.useState("");
 
-function Chat() {
+//array of message
+const [messages, setMessages] = React.useState<Message[]>([]);
 
-	const [inputMessage, setInputMessage] = React.useState("");
+type Message = {
+	inputText: string;
+	//user(sender)
+	//time
+}
 
-	//array of message
-	const [messages, setMessages] = React.useState<Message[]>([]);
-	
-	type Message = {
-		inputText: string;
-		//user(sender)
-		//time
-	}
+// receive input
+const handleInput = (e: React.ChangeEvent<HTMLInputElement>) => {
+	e.preventDefault();
+	console.log(e.target.value);
+	setInputMessage(e.target.value);
+};
 
-	const handleInput = (e: React.ChangeEvent<HTMLInputElement>) => {
-		e.preventDefault();
-		console.log(e.target.value);
-		setInputMessage(e.target.value);
+// take new input string and store to 'Messages' array
+const buildMessage = (e: React.FormEvent<HTMLFormElement>) => {
+	e.preventDefault();
+	if (!inputMessage)
+		return ;
+	const newMessage: Message = {
+		inputText: inputMessage,
 	};
-
-	const handleSend = (e: React.FormEvent<HTMLFormElement>) => {
-		e.preventDefault();
-		if (!inputMessage)
-			return ;
-		const newMessage: Message = {
-			inputText: inputMessage,
-		};
-		setMessages([newMessage, ...messages]);
-		setInputMessage(inputMessage);
-	}
-
+	setMessages([newMessage, ...messages]);
+	setInputMessage(inputMessage);
+}
 	return (
 		<div id="chat-container">
-			<div className="chat-list">
-				<ul>
-					<li>chat room 1</li>
-					<li>chat room 2</li>
-				</ul>
+			
+			{/* left side bar */}
+			<div className="left-sidebar">
+				<ChannelList></ChannelList>
 			</div>
-	
+			{/* main chat window in the middle */}
 			<div className="chat-body">
 				<div className="chat-messages">
 					{messages.map(message => (
@@ -74,23 +78,18 @@ function Chat() {
 						</div>
 					))}
 				</div>
-				<form onSubmit={(e) => handleSend(e)}>
+				{/* input text box */}
+				<form onSubmit={(e) => buildMessage(e)}>
 					<input type="text" onChange={(e) => handleInput(e)} placeholder="type message here"/>
 				</form>
-
 			</div>
 
-			{/* list of users on the right */}
-			<div className="user-list">
-				<ul>
-					<li> user1 </li>
-					<li> user2 </li>
-					<li> user3 </li>
-				</ul>
+			{/* right side bar */}
+			<div className="right-sidebar">
+				<UserList></UserList>
 			</div>
 		</div>
 	);
 }
 
 export default Chat
-
---
+++ front/src/components/Chat/Chat_utils.tsx
@@ -0,0 +1,5 @@
+import { toUnicode } from "punycode";
+import * as React from "react";
+import '../../styles/chat.scss'
+
+
