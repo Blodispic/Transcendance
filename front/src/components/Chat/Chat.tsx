182f56e1b36a30d3043033b95576fb61e8cb2045
Seoyoung Lee <selee@student.42lyon.fr>
2023-01-20 14:38:48 +0100

    basic chat ok; message display problem fixed

--- front/src/components/Chat/Chat.tsx
+++ front/src/components/Chat/Chat.tsx
@@ -3,22 +3,15 @@ import * as React from "react";
 import { io } from "socket.io-client";
 import '../../styles/chat.scss'
 
-// const socket = io("http://localhost:3000") //put backend's port number
 const socket = io("http://" + window.location.hostname + ":4000");
 
-/*
-receive message from back
-emit - sendmessagechannel
-back will broadcast to everyone on channel
-in front, listen for the message on channel
-*/
-
-
-
 function ChannelList() {
+	const chanList = [
+		<li>chan 1</li>,
+		<li>chan 2</li>
+	]
 	return <ul>
-		<li>channel 1</li>
-		<li>channel 2</li>
+		{chanList}
 	</ul>
 }
 
@@ -39,37 +32,24 @@ function UserList() {
 
 function Chat(this: any) {
 
-	const [newInput, setNewInput] = React.useState("");
-	const [messageList, setMessageList] = React.useState<any[]>([]);
-	// const newInput = document.getElementById('newInput');
-	// const messageList = document.getElementById('messageList');
+const [newInput, setNewInput] = React.useState("");
+const [messageList, setMessageList] = React.useState<any[]>([]);
 	
 const handleInput = (e: React.ChangeEvent<HTMLInputElement>) => {
-	e.preventDefault();
+	if (e.target.value == "")
+		return ;
 	setNewInput(e.target.value);
 };
 
-// const buildMessage = (e: React.FormEvent<HTMLFormElement>) => {
-// 	e.preventDefault();
-// 	if (!inputMessage)
-// 		return ;
-// 	const newMessage: Message = {
-// 		inputText: inputMessage,
-// 	};
-// 	setMessageList([newMessage, ...messageList]);
-// 	socket.emit("sendMessage", { inputMessage });
-// 	setInputMessage("");
-// }
-
 const handleSubmitNewMessage = (e: React.FormEvent<HTMLFormElement>) => {
 	e.preventDefault();
-	socket.emit('sendMessage', { newInput });
+	if (newInput != "")
+		socket.emit('sendMessage', { newInput });
 	setNewInput("");
 }
 
-socket.on("recMessage", ( data ) => {
-	console.log({data: newInput});
-	buildNewMessage({data: newInput});
+socket.on('recMessage', ( data ) => {
+	buildNewMessage(data);
 })
 
 const buildNewMessage = (data: any) => {
@@ -83,14 +63,14 @@ const buildNewMessage = (data: any) => {
 			</div>
 			<div className="chat-body">
 				<div className="chat-messages">
-					{messageList.map(chat => (
-						<div key={chat.value} className="__wrap">
-							{chat.value}
+					{messageList.map((chat) => (
+						<div key={chat.newInput} className="__wrap">
+							{chat.newInput}
 						</div>
 					))}
 				</div>
-				<form onSubmit={(e) => handleSubmitNewMessage(e)}>
-					<input type="text" onChange={(e) => handleInput(e)} placeholder="type message here"/>
+				<form id="input_form" onSubmit={(e) => {handleSubmitNewMessage(e); }}>
+					<input type="text" onChange={(e) => {setNewInput(e.target.value)}} placeholder="type message here" value={newInput}/>
 				</form>
 
 			</div>
