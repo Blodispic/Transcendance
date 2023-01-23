d3b6c371040f20d4749a4c6441ceaa690b753e78
Blodispic <75845611+Blodispic@users.noreply.github.com>
2023-01-23 17:01:10 +0000

    Added MatchMaking

---
+++ back/src/game/entities/game.entity.ts
@@ -0,0 +1,14 @@
+import { Entity, Column, ManyToOne, PrimaryGeneratedColumn } from "typeorm";
+import { User } from "src/user/entities/user.entity";
+
+@Entity()
+export class Game {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @ManyToOne(type => User)
+  player1: User;
+
+  @ManyToOne(type => User)
+  player2: User;
+}
