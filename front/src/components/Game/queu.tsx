e912cb17082b1bfa82f718fe62b8f57439bf6cea
emmanuel-lbs <59801700+emmanuel-lbs@users.noreply.github.com>
2023-01-24 12:04:12 +0100

    adamfucntion in button click

--- front/src/components/Game/queu.tsx
+++ front/src/components/Game/queu.tsx
@@ -3,29 +3,33 @@ import { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
 import { useAppSelector } from '../../redux/Hook';
 
+default function function_adam() {
+
+
+}
 
 export default function Queu() {
 
-  const myUser = useAppSelector(state => state.user);
+    const myUser = useAppSelector(state => state.user);
 
 
-  useEffect(() => {
-            const fetchuser = async () => {
-                    const response = await fetch(`http://localhost:4000/game/`, {
-                            method: 'POST',
-                            body: JSON.stringify({ id: myUser.user!.id }),
-                    })
-            }
-            fetchuser()
-}, [])
+    useEffect(() => {
+        const fetchuser = async () => {
+            const response = await fetch(`http://localhost:4000/game/`, {
+                method: 'POST',
+                body: JSON.stringify({ id: myUser.user!.id }),
+            })
+        }
+        fetchuser()
+    }, [])
 
     return (
-        <button className="center pulse pointer" >
+        <button className="center pulse pointer" onClick={(e) => function_adam(e)} >
             <Link to="/Game/">
                 <a>
                     play at foot-pong
                 </a>
             </Link>
         </button>
-        )
+    )
 }
