dabfe8779866a49265d0a6c0e24b3da38e6622a3
selee <selee@student.42lyon.fr>
2023-02-07 16:50:43 +0100

    result popup

--- front/src/components/Game/Result.tsx
+++ front/src/components/Game/Result.tsx
@@ -1,22 +1,26 @@
 import React from 'react';
 import { useEffect, useState } from 'react';
-import { Link } from 'react-router-dom';
+import { Link, Navigate, useNavigate } from 'react-router-dom';
 import "../../styles/game.scss";
 
 export function Victory() {
+	const navigate = useNavigate();
 	return (
-		<h1>
-			"Congratulations you Won!"
-		</h1>
+		<div className='result-popup'style={{zIndex: 100}}>
+			<div className='result-popup-inner' onClick={_ => navigate("/Game") }>
+				Congratulations! <br />You won!
+			</div>
+		</div>
 	);
 }
 
 export function Defeat() {
-
-
+	const navigate = useNavigate();
 	return (
-		<h1>
-			"Congratulations you Lost!"
-		</h1>
+		<div className='result-popup' style={{zIndex: 100}}>
+			<div className='result-popup-inner' onClick={_ => navigate("/Game")}>
+				Congratulations! <br />You lost!
+			</div>
+		</div>
 	);
 }
