fb8e0a5d1836200ce051b552f0cded8f3c6dce27
Emmanuel Labasque <elabasqu@z1r8p3.42lyon.fr>
2023-02-07 13:14:23 +0100

    onglet component for profile page for selected history or friend request or 2fa

--- front/src/components/Profile/Profile.tsx
+++ front/src/components/Profile/Profile.tsx
@@ -7,30 +7,55 @@ import { ImCheckmark, ImCross } from "react-icons/im";
 import { Header } from './Header_profile';
 import { Friends } from './friend_request';
 import { page } from '../../interface/enum';
+import { History } from './History';
 
 
+function Onglets(props: { current: page, setOnglets: Function }) {
+
+        const {current, setOnglets } = props;
+        return (
+                <div className='onglets'>
+                        <button className={`pointer ${current === page.PAGE_1 ? "" : "not-selected"}`}
+                         onClick={e => setOnglets(page.PAGE_1)}>
+                                <a >
+                                        history
+                                </a>
+                        </button>
+                        <button className={`pointer ${current === page.PAGE_2 ? "" : "not-selected"}`}
+                        onClick={e => setOnglets(page.PAGE_2)} >
+                                <a >
+                                        friend_request
+                                </a>
+                        </button> <button className={`pointer ${current === page.PAGE_3 ? "" : "not-selected"}`}
+                         onClick={e => setOnglets(page.PAGE_3)}>
+                                <a >
+                                        2fa
+                                </a>
+                        </button>
+                </div>
+        )
+}
 export default function Profile() {
         const [currentUser, setCurrentUser] = useState<IUser | undefined>(undefined);
         let avatar: string = "";
         let { id } = useParams();
-        const [pages, setpages] = useState<page>(page.PAGE_1);
+        const [pages, setPages] = useState<page>(page.PAGE_1);
+
         useEffect(() => {
-
-
-
                 if (id) {
                         const fetchid = async () => {
                                 const response = await fetch(`${process.env.REACT_APP_BACK}user/id/${id}`, {
                                         method: 'GET',
                                 })
-
                                 setCurrentUser(await response.json());
                         }
                         fetchid()
-
-
                 }
         }, [id])
+
+        useEffect(() => {
+
+        }, [Onglets])
 
 
         if (currentUser === undefined || avatar === undefined) {
@@ -40,24 +65,26 @@ export default function Profile() {
                         </div>
                 );
         }
+
         return (
                 <div className='all'>
                         {currentUser &&
                                 <Header currentUser={currentUser} setCurrentUser={setCurrentUser} />
                         }
                         <div className='container'>
-                        {
-                                pages == page.PAGE_1 &&
-                                <Friends user={currentUser} />
-                        }
-                        {
-                                pages == page.PAGE_2 &&
-                                <Friends user={currentUser} />
-                        }
-                        {
-                                pages == page.PAGE_3 &&
-                                <Friends user={currentUser} />
-                        }
+                                <Onglets current={pages} setOnglets={setPages} />
+                                {
+                                        pages == page.PAGE_1 &&
+                                        <History />
+                                }
+                                {
+                                        pages == page.PAGE_2 &&
+                                        <Friends user={currentUser} />
+                                }
+                                {
+                                        pages == page.PAGE_3 &&
+                                        <Friends user={currentUser} />
+                                }
 
                         </div>
                 </div>
