b2973e8ef0e6492ef249aefcf66493ddd0c214c4
Adam Cusanno <acusanno@z1r7p5.42lyon.fr>
2023-02-14 09:12:49 +0100

    Added createCustomRoom in the front, it's now possible to launch a custom game(with yourself) but it's still impossible to choose the custom parameters(To send from the front to the back)

--- back/src/game/pong.gateway.ts
+++ back/src/game/pong.gateway.ts
@@ -61,32 +61,33 @@ export class PongGateway implements OnGatewayDisconnect, OnGatewayInit {
 	}
 
 	@SubscribeMessage("createCustomGame")
-	HandleCustomGame(@MessageBody() user1: User, user2: User, extra: boolean, scoreMax: number, @ConnectedSocket() client: Socket)
+	HandleCustomGame(@MessageBody() payload: any, @ConnectedSocket() client: Socket)
 	{
-		console.log("Add " + user1.username + " to custom game.");
-		console.log("Add " + user2.username + " to custom game.");
+		console.log("Add " + payload.user1.username + " to custom game.");
+		console.log("Add " + payload.user2 + " to custom game.");
 		let userSocket1 : any = userList[0]; //By default both user are the first user of the list
 		let userSocket2 : any = userList[0]; //By default both user are the first user of the list
 		let i : number = 0;
 		while (i < userList.length)
 		{
-			if (userList[i].handshake.auth.user.username === user1.username)
+			if (userList[i].handshake.auth.user.username === payload.user1.username)
 			{
 				userSocket1 = userList[i];
+				break;
 			}
 			i++;
 		}
 		i = 0;
 		while (i < userList.length)
 		{
-			if (userList[i].handshake.auth.user.username === user2.username)
+			if (userList[i].handshake.auth.user.username === payload.user2.username)
 			{
 				userSocket2 = userList[i];
+				break;
 			}
 			i++;
 		}
-		this.gameService.startCustomGame(this.server, userSocket1, userSocket2, extra, scoreMax);
-		
+		this.gameService.startCustomGame(this.server, userSocket1, userSocket2, payload.extra, payload.scoreMax);
 	}
 
 	@SubscribeMessage("GameEnd")
--- front/src/components/Game/Queue.tsx
+++ front/src/components/Game/Queue.tsx
@@ -5,6 +5,7 @@ import { Player } from './Game';
 import { useNavigate } from "react-router-dom";
 import { socket } from '../../App';
 import { debug } from 'console';
+import { logDOM } from '@testing-library/react';
 
 export default function Queue() {
 
@@ -16,10 +17,11 @@ export default function Queue() {
         return;
     }
 
-    function createCustomRoom() {
-        socket.emit("addToWaitingRoom", myUser.user);
-        return;
-    }
+	function createCustomRoom() {
+		console.log("Create custom", myUser.user)
+		socket.emit("createCustomGame", {user1: myUser.user, user2: myUser.user, extra: true, scoreMax: 10});
+		return;
+	}
 
     socket.on("RoomStart", (roomId: number, player: Player) => {
         navigate("/game/" + roomId, { state: { Id: roomId } });
@@ -37,18 +39,22 @@ export default function Queue() {
     }, [])
 
     return (
-        <div>
-            <button className="center pulse pointer" onClick={(e) => addToWaitingRoom()} >
-                <Link to="/Game/">
-                        Add to Waiting Room
-                </Link>
-            </button>
+        <div id="queue-container">
 
-            <button className="center pulse pointer" onClick={(e) => createCustomRoom()} >
-            <Link to="/Game/">
-                    Create Custom Room
-            </Link>
-            </button>
+            <div>
+                <button onClick={(e) => addToWaitingRoom()} >
+                   <Link to="/Game/">
+                           Add to Waiting Room
+                  </Link>
+                </button>
+            </div>
+            <div>
+                <button onClick={(e) => createCustomRoom()} >
+                <Link to="/Game/">
+                       Create Custom Room
+                </Link>
+                </button>
+            </div>
         </div>
     )
 }
