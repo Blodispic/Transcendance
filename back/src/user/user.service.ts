a8f78f5af81c7950b9b2e31b5302de2a76aeca59
Blodispic <75845611+Blodispic@users.noreply.github.com>
2023-02-21 17:10:58 +0100

    Check username in Update

--- back/src/user/user.service.ts
+++ back/src/user/user.service.ts
@@ -153,8 +153,16 @@ export class UserService {
     if (user) {
       //Si vous voulez plus de chose a update, mettez le dans le body et faites un if
       if (userUpdate.username)
-        user.username = userUpdate.username;
-      if (userUpdate.status)
+      {
+        const checkUsername = await this.usersRepository.findOneBy({
+          username: userUpdate.username,
+        })
+        if ( checkUsername )
+          throw new NotFoundException("Username exists");
+        else
+          user.username = userUpdate.username;
+      }
+        if (userUpdate.status)
         user.status = userUpdate.status;
       if (userUpdate.twoFaEnable)
         user.twoFaEnable = userUpdate.twoFaEnable
