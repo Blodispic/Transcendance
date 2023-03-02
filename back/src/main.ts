da1f25efd4a7fc2e2f6c958fc37f9542790b0aa6
Blodispic <zhouromain@hotmail.fr>
2023-03-02 12:15:50 +0100

    Small fixes

--- back/src/main.ts
+++ back/src/main.ts
@@ -1,14 +1,18 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import * as cookieParser from 'cookie-parser';
+import * as dotenv from 'dotenv';
 
 async function bootstrap() {
+  dotenv.config();
+
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: 'http://10.11.8.1:3000',
+    origin: process.env.FRONT_URL,
     credentials: true,
   });
   app.use(cookieParser());
   await app.listen(4000);
 }
+
 bootstrap();
