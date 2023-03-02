3c1617eee767a91eb75b787eb48309bd2b326af5
Blodispic <zhouromain@hotmail.fr>
2023-03-02 12:56:51 +0100

    stategy -> strategy

--- back/src/user/user.module.ts
+++ back/src/user/user.module.ts
@@ -7,7 +7,7 @@ import { Results } from '../results/entities/results.entity';
 import { FriendRequest } from './entities/friend-request.entity';
 import { JwtModule } from '@nestjs/jwt';
 import { jwtConstants } from 'src/Oauth/constants';
-import { JwtStrategy } from 'src/Oauth/jwt.stategy';
+import { JwtStrategy } from 'src/Oauth/jwt.strategy';
 
 @Module({
   controllers: [UserController],
