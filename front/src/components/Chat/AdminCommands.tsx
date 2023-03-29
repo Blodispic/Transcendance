44bd432b216cbf72c206413a224db246c5494f54
emmanuel-lbs <emmanuel.labasque@gmail.com>
2023-03-29 16:25:50 +0200

    beco a deja ca normalelemnt

--- front/src/components/Chat/AdminCommands.tsx
+++ front/src/components/Chat/AdminCommands.tsx
@@ -18,13 +18,13 @@ export function BanUser(props: { chanid: any, userid: any, trigger: boolean, set
 		else
 			socket.emit('BanUser', { chanid: props.chanid, userid: props.userid, timeout: parseInt(timeout) * 1000 });
 	}
-	
+
 	useEffect(() => {
 		socket.on("banUserFailed", (errorMessage) => {
 			setFailed(true);
 			setError(errorMessage);
 		});
-		socket.on("banUserOK", ({chanid, userid}) => {
+		socket.on("banUserOK", ({ chanid, userid }) => {
 			props.setTrigger(false);
 			setFailed(false);
 		});
@@ -71,7 +71,7 @@ export function MuteUser(props: { chanid: any, userid: any, trigger: boolean, se
 			setFailed(true);
 			setError(errorMessage);
 		});
-		socket.on("muteUserOK", ({chanid, userid}) => {
+		socket.on("muteUserOK", ({ chanid, userid }) => {
 			props.setTrigger(false);
 			setFailed(false);
 		});
@@ -114,11 +114,12 @@ export function ConfigureChannelPrivate(props: { trigger: boolean, setTrigger: (
 	const AddPeoplePrivate = () => {
 		if (allfriend.length > 0)
 			socket.emit('AddPeoplePrivate', { chanId: props.channel.id, users: allfriend.map(user => user.id) });
+		cleanlist();
 	}
 	const cleanlist = () => {
 		setAlluser([]);
 		setAllFriend([]);
-		setAlreadyhere([])
+		// setAlreadyhere([])
 		setMyvar(false)
 	}
 
@@ -139,7 +140,7 @@ export function ConfigureChannelPrivate(props: { trigger: boolean, setTrigger: (
 		return () => {
 			socket.off("AddPeoplePrivateOk");
 		}
-	}, []);
+	}, [props.channel.users]);
 	const get_all = async () => {
 		const response = await fetch(`${process.env.REACT_APP_BACK}user`, {
 			method: 'GET',
@@ -151,23 +152,23 @@ export function ConfigureChannelPrivate(props: { trigger: boolean, setTrigger: (
 		const data = await response.json();
 		setAlluser(
 			data
-			  .filter((user: { username: string, status: string}) => 
-				user.username !== myUser.user.user?.username && user.status === "Online"
-			  )
-			  .filter((user: { id: number }) => 
-				alreadyhere.findIndex((alreadyhereuser: IUser) => alreadyhereuser.id === user.id) === -1
-			  )
-			  .filter((user: { id: number }) => 
-			  allfriend.findIndex((allfriend: IUser) => allfriend.id === user.id) === -1
-			)
-			
-		  );
+				.filter((user: { username: string, status: string }) =>
+					user.username !== myUser.user.user?.username && user.status === "Online"
+				)
+				.filter((user: { id: number }) =>
+					alreadyhere.findIndex((alreadyhereuser: IUser) => alreadyhereuser.id === user.id) === -1
+				)
+				.filter((user: { id: number }) =>
+					allfriend.findIndex((allfriend: IUser) => allfriend.id === user.id) === -1
+				)
+
+		);
 	}
 
 	return (props.trigger) ? (
-		<div className="chat-form-popup" onClick={_ => {cleanlist(); props.setTrigger(false)}}>
-			<div className="chat-form-inner" onClick={e => {e.stopPropagation()}}>
-				<HiOutlineXMark className="close-icon" onClick={_ => {cleanlist();props.setTrigger(false)}} /> <br />
+		<div className="chat-form-popup" onClick={_ => { cleanlist(); props.setTrigger(false) }}>
+			<div className="chat-form-inner" onClick={e => { e.stopPropagation() }}>
+				<HiOutlineXMark className="close-icon" onClick={_ => { cleanlist(); props.setTrigger(false) }} /> <br />
 				<h3>Edit members</h3>
 				<div className='allpoeple'>
 					{
@@ -257,20 +258,20 @@ export function ConfigureChannel(props: { trigger: boolean, setTrigger: (value: 
 					</>
 				}
 				{
-					props.channel.banned.length > 0 && 
+					props.channel.banned.length > 0 &&
 					<>
-					<h3>Unban user</h3>
-					{props.channel.banned?.map(banned => (
-						<ul key={banned.id}>
-							<li title='Unban'>
-							<div className='avatar-inpopup'>
+						<h3>Unban user</h3>
+						{props.channel.banned?.map(banned => (
+							<ul key={banned.id}>
+								<li title='Unban'>
+									<div className='avatar-inpopup'>
 
-							<img className="cursor-onsomoene avatar avatar-manu" src={`${process.env.REACT_APP_BACK}user/${banned.id}/avatar`} alt="" onClick={() => handleUnban()}/>
-								</div>
-							</li>
+										<img className="cursor-onsomoene avatar avatar-manu" src={`${process.env.REACT_APP_BACK}user/${banned.id}/avatar`} alt="" onClick={() => handleUnban()} />
+									</div>
+								</li>
 
-						</ul>
-					))}
+							</ul>
+						))}
 					</>
 				}
 				<button onClick={setPassword}> Save Setting </button>
